2d9c388c3fef6bc82adfe4b410977e6023bd403f
vincent <sec1995@hotmail.com>
2020-09-23 22:07:32 +0800

    添加SpringCloudStream-RocketMQ引用

--- src/main/java/com/alibaba/learning/LearningApplication.java
+++ src/main/java/com/alibaba/learning/LearningApplication.java
@@ -3,9 +3,12 @@ package com.alibaba.learning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableBinding(Source.class)
 public class LearningApplication {
 
 	public static void main(String[] args) {
